7228cd2d58fea20046b4e71ca86745aa8e184792
lenovor <raieshg@gmail.com>
2021-04-26 22:52:25 +0545

    add member to database completed

---
+++ StockManagement/AddMember.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddMember.aspx.cs" Inherits="StockManagement.AddMember" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add Member" />
+</asp:Content>
---
+++ StockManagement/AddMember.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StockManagement
+{
+    public partial class AddMember : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+           string connectionstring = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            SqlConnection mySqlConnection = new SqlConnection(connectionstring);
+          
+                mySqlConnection.Open();
+                SqlCommand cmd = new SqlCommand("Insert into dbo.Member values(@MemberNumber,@MemberName,@Address,@ContactNumber,@Email,@MemberType)", mySqlConnection);
+                cmd.Parameters.AddWithValue("@MemberNumber", "1");
+                cmd.Parameters.AddWithValue("@MemberName", "Raiesh Ghimire");
+                cmd.Parameters.AddWithValue("@Address", "Sinamangal-9, Kathmandu");
+                cmd.Parameters.AddWithValue("@ContactNumber", "98601200042");
+                cmd.Parameters.AddWithValue("@Email", "raieshg@gmail.com");
+            cmd.Parameters.AddWithValue("@MemberType", "Exclusive");
+            cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+
+
+
+           
+         
+        }
+    }
+    }
