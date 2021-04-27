00f13f135af538549a9bc55bdbf36d87d73a99bc
lenovor <raieshg@gmail.com>
2021-04-27 08:57:23 +0545

    itemstock completed

---
+++ StockManagement/ItemStock.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ItemStock.aspx.cs" Inherits="StockManagement.ItemStock" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:StockManagementConnectionString %>" SelectCommand="SELECT * FROM [Stock]"></asp:SqlDataSource>
+    <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="ItemCode" DataValueField="ItemCode">
+    </asp:DropDownList>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+     <table width="100%" align="center" cellpadding="2" cellspacing="2" border="0" bgcolor="#EAEAEA" >
+        <tr align="left" style="background-color:#004080;color:White;" >
+            <td> Stock </td>                        
+            <td> Quantity </td>            
+            <td>Date</td> 
+            <td>Status</td>
+        </tr>
+
+        <%=getWhileLoopData()%>
+
+    </table>
+</asp:Content>
---
+++ StockManagement/ItemStock.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StockManagement
+{
+    public partial class ItemStock : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getWhileLoopData();
+        }
+        public string getWhileLoopData()
+        {
+            string dropdown = DropDownList1.SelectedValue.ToString();
+            string connectionstring = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            SqlConnection mySqlConnection = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("Select * from dbo.Stock where ItemCode='"+dropdown+"'", mySqlConnection);
+            mySqlConnection.Open();
+            cmd.Connection = mySqlConnection;
+           
+            string data = "";
+
+            using (SqlDataReader QueryReader = cmd.ExecuteReader())
+            {
+                if (QueryReader.HasRows)
+                {
+
+                    while (QueryReader.Read())
+                    {
+                        string itemCode = QueryReader.GetString(1);
+                        int Quantity = QueryReader.GetInt32(2);
+                        string StockPurchaseDate = QueryReader.GetString(3);
+                        string status = "";
+                        if (Quantity < 10)
+                        {
+                            status = "Running out of stock";
+                        }
+                        else if (Quantity == 0)
+                        {
+                            status = "Out of  Stock";
+                        }
+                        else {
+                            status = "Available in Stock";
+                        }
+
+                        data += "<tr><td> " + itemCode + "</td><td> " + Quantity+ "</td><td> "+StockPurchaseDate+ "</td><td> " + status + "</td><tr> ";
+                    }
+                    mySqlConnection.Close();
+
+                }
+                return data;
+            }
+        }
+    }
+
+}
